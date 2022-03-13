ed2d2fa6b008633bbc98615b430a5c680225dd48
KrisLopez98 <87205060+KrisLopez98@users.noreply.github.com>
2022-03-12 21:11:27 -0600

    Initial commit

---
+++ app/src/main/java/com/example/miedadcanina/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.miedadcanina
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.miedadcanina.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val ageEdit = binding.ageEdit
+        val resultText = binding.resultText
+        val button = binding.button
+
+        Log.d("MainActivity","Activity created")
+
+        button.setOnClickListener {
+            val ageString : String = ageEdit.text.toString()
+            if(ageString.isNotEmpty()) {
+                val ageInt: Int = ageString.toInt()
+                val result: Int = ageInt * 7
+                resultText.text = getString(R.string.result_text, result)
+            }else{
+                Log.d("MainActivity", "Age string is empty")
+                Toast.makeText(this, R.string.you_must_insert_your_age, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
